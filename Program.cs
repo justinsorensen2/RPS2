db0d350f3650a6c68617b2347addb06dd14c3722
justinsorensen2dotnet <>
2020-05-05 12:34:01 -0400

    Recompleted explorer mode.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace RPS2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+
+      //create var to manage while loop
+      var playing = true;
+      //create var to hold invalid entry string
+      var invalid = "That is not a valid selection.";
+      //var to hold valid rps selections
+      var rps = "Valid entries are rock, paper, or scissors.";
+      //var to hold valid y or n
+      var yOrN = "Valid entries are y (yes) or n (no).";
+
+      while (playing)
+      {
+        Console.WriteLine("Player, please enter your selection.");
+        Console.WriteLine($"{rps}");
+
+        //set var to hold player choice
+        var playerSelection = Console.ReadLine().ToLower();
+        //error catching
+        if (playerSelection == "rock" && playerSelection != "paper" && playerSelection != "scissors")
+        {
+          Console.WriteLine($"{invalid}");
+          Console.WriteLine($"{rps} Please try again.");
+          playerSelection = Console.ReadLine().ToLower();
+        }
+        //create var to hold computer selection
+        var cpuSelection = "RPS";
+        //create new random instance
+        Random rando = new Random();
+        //set output for rando
+        int randOutput = rando.Next(1, 3);
+        //set cpuSelection based on rando
+        if (randOutput == 1)
+        {
+          cpuSelection = "rock";
+        }
+        else if (randOutput == )
+        {
+          cpuSelection = "paper";
+        }
+        else
+        {
+          cpuSelection = "scissors";
+        }
+
+        Console.WriteLine($"Player selected {playerSelection}. Computer selected {cpuSelection}.");
+        //compare selections and display winner
+        //player loss conditions
+        if ((cpuSelection == "rock" && playerSelection == "scissors") || (cpuSelection == "paper" && playerSelection == "rock") || (cpuSelection == "scissors" && playerSelection == "paper"))
+        {
+          Console.WriteLine("Player lost.");
+          //tie conditions
+        }
+        else if ((cpuSelection == "scissors" && playerSelection == "scissors") || (cpuSelection == "rock" && playerSelection == "rock") || (cpuSelection == "paper" && playerSelection == "paper"))
+        {
+          Console.WriteLine("Tie game.");
+        }
+        else
+        {
+          Console.WriteLine("Player wins!");
+        }
+
+
+        Console.WriteLine("Would you like to play again?");
+        Console.WriteLine($"{yOrN}");
+        var playAgain = Console.ReadLine().ToLower();
+        //error catching
+        if (playAgain != "y" && playAgain != "n")
+        {
+          Console.WriteLine($"{invalid}");
+          Console.WriteLine($"{yOrN}");
+          playAgain = Console.ReadLine().ToLower();
+        }
+        //determine what to do based on input - replay or not
+        if (playAgain == "y")
+        {
+          Console.WriteLine("Ok. On to the next round.");
+        }
+        else
+        {
+          Console.WriteLine("Thank you for playing. Goodbye.");
+          playing = false;
+        }
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+Explorer Mode
+
+As a user, I should see a welcome message.
+As a user, I should see the options I can pick (i.e., "rock", "paper", "scissor")
+The computer should randomly decide one of the options.
+You program should then decide who the winner is;
+The user should then see the randomly selected option, as well as a win or lose message.
+
+    add some validation around user input.
+
+Adventure Mode
+
+Create a way for the user to choose a level of difficulty, normal, impossible, or easy.
+
+    normal should run the game as in explorer mode above
+    impossible mode always chooses the winning option (if the user picked paper, the computer always select scissor)
+    easy mode always chooses the losing option (if the user picked paper, then the computer always select rock)
+
+    Allow the user to play again. hint: while loops
+
+Epic Mode
+
+You have read about rock, paper, scissors, but have you tried, rock paper scissors lizard spock. Modify your game to allow the extra options
+Your game is currently only 1 vs. 1, add the ability to add more computer or users players to the game
